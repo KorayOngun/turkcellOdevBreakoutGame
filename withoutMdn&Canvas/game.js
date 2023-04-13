80c422028b94c3815aa35769e2fa50b931c3ee69
koray öngün <koraygg28@gmail.com>
2023-04-14 00:12:25 +0300

    platemove

--- withoutMdn&Canvas/game.js
+++ withoutMdn&Canvas/game.js
@@ -6,17 +6,49 @@ var game_right = 720;
 var ball_x = 50;
 var ball_y = 50;
 var ball = document.createElement("div");
+ball.id = "ball"
+gameDiv.appendChild(ball);
 var dX = 1;
 var dY = 1;
-function createBall() {
-  gameDiv.appendChild(ball);
-  ball.style.width = "18px";
-  ball.style.position = "absolute";
-  ball.style.height = "18px";
-  ball.style.backgroundColor = "red";
-  ball.style.borderRadius = "50%";
+var plate = document.createElement("div");
+var plate_left = 0
+var isLeft = false
+var isRight = false
+var isMove = false
+plate.id = "plate"
+gameDiv.appendChild(plate)
+
+
+document.addEventListener("keydown",keyDownHandler)
+document.addEventListener("keyup",keyUpHandler)
+
+function keyDownHandler(e) {
+  if (e.key == "Left" || e.key == "ArrowLeft") {
+    isLeft = true
+  }
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    isRight = true
+  } 
+  isMove = true
 }
-createBall();
+function keyUpHandler(e) {
+  if (e.key == "Left" || e.key == "ArrowLeft") {
+    isLeft = false
+  }
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    isRight = false
+  } 
+  isMove = false
+}
+
+function movePlate() {
+  if (isLeft && plate_left>0) {
+    plate_left = plate_left-2
+  }else if(isRight && plate_left<570){
+    plate_left = plate_left+2
+  }
+  plate.style.left = `${plate_left}px`
+}
 
 function moveBall() {
   if (ball_x == 702) {
@@ -37,10 +69,34 @@ function moveBall() {
   ball.style.left = `${ball_x}px`;
   ball.style.top = `${ball_y}px`;
 }
+function collisionDetection() {
+ plateCollision();
+}
+function plateCollision() {
+  if (ball_y==462) {
+    alert("gameover")
+  }
+  if (ball_y>458) {
+    if (plate_left<ball_x && plate_left+150>ball_x) {
+      dY = -1
+      if (isMove) {
+        if (isLeft) {
+          dX = -1
+        }else{
+          dX = 1
+        }
+      }
+    }
+  }
+}
+
+
+
 
 function game() {
   moveBall();
-  
+  movePlate();
+  collisionDetection()
 }
 
 setInterval(game, 1);
