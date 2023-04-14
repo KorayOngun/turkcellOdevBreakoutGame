f61e22144edf1653d6069a002e8c3709ef291662
koray öngün <koraygg28@gmail.com>
2023-04-14 15:27:45 +0300

    addBricks

--- withoutMdn&Canvas/game.js
+++ withoutMdn&Canvas/game.js
@@ -4,11 +4,12 @@ var game_bottom = 480;
 var game_left = 0;
 var game_right = 720;
 var ball_x = 50;
-var ball_y = 50;
+var ball_y = 200;
 var ball = document.createElement("div");
 ball.id = "ball"
+
 gameDiv.appendChild(ball);
-var dX = 1;
+var dX = 0;
 var dY = 1;
 var plate = document.createElement("div");
 var plate_left = 0
@@ -17,10 +18,31 @@ var isRight = false
 var isMove = false
 plate.id = "plate"
 gameDiv.appendChild(plate)
-
+const brickRow = 3
+const brickColumn = 6
+var bricks = [] 
+var c = 1
+function createBricks() {
+    for (let i = 0; i < brickRow; i++) {
+      for (let j = 0; j < brickColumn; j++) {
+          var brick = document.createElement("div")
+          brick.className = "brick"
+          brick.id = c 
+          c = c + 1;     
+          brick.style.top = `${20+(30*i)}px`
+          brick.style.backgroundColor = "black";
+          brick.style.left = `${10+(120*j)}px`
+          bricks.push(brick)
+          gameDiv.appendChild(brick)  
+      }
+    }
+}
+createBricks()
 
 document.addEventListener("keydown",keyDownHandler)
 document.addEventListener("keyup",keyUpHandler)
+
+
 
 function keyDownHandler(e) {
   if (e.key == "Left" || e.key == "ArrowLeft") {
@@ -51,10 +73,10 @@ function movePlate() {
 }
 
 function moveBall() {
-  if (ball_x == 702) {
+  if (ball_x > 702) {
     dX = -1;
   }
-  if (ball_x == 0) {
+  if (ball_x < 0) {
     dX = 1;
   }
   if (ball_y == 462) {
@@ -71,6 +93,7 @@ function moveBall() {
 }
 function collisionDetection() {
  plateCollision();
+ brickCollision();
 }
 function plateCollision() {
   if (ball_y==462) {
@@ -81,15 +104,34 @@ function plateCollision() {
       dY = -1
       if (isMove) {
         if (isLeft) {
-          dX = -1
+          dX = dX-1 
         }else{
-          dX = 1
+          dX = dX+1
         }
       }
     }
   }
 }
-
+function brickCollision() {
+  var brick = bricks.find(findBrick)
+  if (brick) {
+    brick.style.backgroundColor = "aqua";
+  }
+  
+}
+function findBrick(b) {
+    if(b.style.backgroundColor == "black"){
+      if (b.offsetTop == ball_y && b.offsetLeft < ball_x && b.offsetLeft+100>ball_x) {
+        dY = 1
+        return true
+      }if (b.offsetTop-10 == ball_y && b.offsetLeft < ball_x && b.offsetLeft+100>ball_x )  {
+        dY = -1
+        return true
+      }
+      c = c -1
+    }   
+    return false
+}
 
 
 
